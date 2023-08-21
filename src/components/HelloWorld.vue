48c2bc3bf0a93cde778cad869b5775adb3254269
alessandra martera <ale.martera@gmail.com>
2023-08-21 18:12:00 +0200

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'HelloWorld',
+}
+</script>
+
+<template>
+    <h1>
+        Hello
+    </h1>
+</template>
+
+<style scoped></style>
