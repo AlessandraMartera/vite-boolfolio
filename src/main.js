26c6464457278c53fb038e8f792dfef191bc48c8
alessandra martera <ale.martera@gmail.com>
2023-08-22 16:10:12 +0200

    router componets linked

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 // import router
-import { router } from "./router.js";
+import { router } from "./router.js"
 
 // import bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
--- src/router.js
+++ src/router.js
@@ -1,17 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-
+import AppHome from './pages/AppHome.vue';
 import About from './pages/About.vue';
- 
+
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        {
-            path: '/',
-            name: 'about',
-            component: About
-        }
-    ]
+history: createWebHistory(),
+routes: [
+{
+path: '/',
+name: 'home',
+component: AppHome
+},
+{
+path: '/about',
+name: 'about',
+component: About
+},
+]
 });
- 
 export { router };
